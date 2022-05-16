c8c0c130be3f2ce0777e80ad485eea294a759ae8
rmcstt <anisio.nanato@hotmail.com>
2022-05-15 21:32:16 -0300

    o.o.

---
+++ OO.js
@@ -0,0 +1,28 @@
+//codigo nao executavel
+
+//procedural
+
+processamento(valor1, valor2, valor3)
+
+// O.O.ppp
+
+objeto = {
+  valor1,
+  valor2,
+  valor3,
+  proccessamento() {}
+}
+
+objeto.proccessamento() // foco passou a ser o objeto
+
+/*
+objeto = capsula que agrupa comportamentos e atributos
+principio de "emcapsulamento
+
+principios importantes
+1 * abstração
+2 * emcapsulamento 
+3 * herança (prototype)
+4 * polimorfismo
+  */
+//favoreça mais a composição do que a herança
